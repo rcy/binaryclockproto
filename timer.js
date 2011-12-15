df97ec7884352e98ad2e437c4fbe577ea9fd7e17
Ryan Yeske <rcyeske@gmail.com>
2011-12-15 02:46:17 -0800

    mostly working

--- timer.js
+++ timer.js
@@ -1,4 +1,5 @@
-function draw(number) {
+function draw() {
+  var number = g_num;
   var tens = number / 10;
   var ones = number % 10;
 
@@ -14,13 +15,56 @@ function draw(number) {
   if (tens & 4) $("#tens .4").addClass("on");
   if (tens & 2) $("#tens .2").addClass("on");
   if (tens & 1) $("#tens .1").addClass("on");
+
+  $("#human").html(number);
 }
 
-var num = 60;
+var g_num = 60;
 
-setInterval(function() {
-  draw(num);
-  num -= 1;
-  if (num < 0)
-    num = 60;
-}, 1000);
+draw();
+
+$("button#up").on("click", function() {
+  g_num++;
+  draw();
+});
+
+$("button#down").on("click", function() {
+  g_num--;
+  draw();
+});
+
+var g_running = false;
+var g_timer;
+var g_initial;
+
+$("button#startstop").on("click", function() {
+  if (!g_running)
+    start();
+  else
+    stop();
+});
+
+function start() {
+  g_initial = g_num;
+  g_timer = setInterval(function() {
+    g_num -= 1;
+    draw();
+    if (g_num <= 0) {
+      stop();
+      buzz();
+      reset();
+      draw();
+    }
+  }, 1000);
+  g_running = true;
+}
+function stop() {
+  clearTimeout(g_timer);
+  g_running = false;
+}
+function reset() {
+  g_num = g_initial;
+}
+function buzz() {
+  alert("times up!");
+}
